55c0137bad638894c7ef562432f34148f7c5f7de
lorosanu <lorosanu@users.noreply.github.com>
2016-06-02 10:06:24 +0200

    Add an optional argument to the script with a default value

--- langstat.sh
+++ langstat.sh
@@ -1,40 +1,89 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
+#======================================
+# process the command line arguments
+#======================================
+
+if [ $# -ne 1  ] && [ $# -ne 2 ]
 then
-    echo "Usage: $0 <filename>"
-    echo "<filename> : the dictionary's filename"
+    echo "Description : output statistics on the letters present in a dictionary file"
+    echo ""
+    echo "Usage: $0 <file> --opt={alpha, num, freq, hist, wlength}"
+    echo "<file> : the dictionary's filename"
+    echo "--opt  : optional argument, the display mode."
+    echo "         Choose a value from : "
+    echo "          - 'alpha'   = sort in alphabetic order of letters (* by default)"
+    echo "          - 'num'     = sort in decreasing order the number of occurrences for each letter"
+    echo "          - 'freq'    = sort in decreasing order the frequency of occurrences for each letter"
+    echo "          - 'hist'    = draw a horizontal 'histogram' on the frequency of occurrences for each letter"
+    echo "          - 'wlength' = statistics on the words length"
     echo ""
     exit
-else
-    dico_file=$1
 fi
 
-
-if [ ! -e $dico_file ]
+if [ $# -ge 1 ]
 then
-    echo "Error: the given dictionary file does not exist."
-    exit
+    dico_file=$1
+
+    if [ ! -e $dico_file ]
+    then
+        echo "Error: the given dictionary file does not exist."
+	exit
+    fi
+
+    display_opt="alpha"		# default display option
+
+    if [ $# -eq 2 ]
+    then
+        arg=$2
+
+	if [[ $arg == --opt=* ]] && ( [[ $arg == *alpha ]] || [[ $arg == *num ]] || [[ $arg == *freq ]] || [[ $arg == *hist ]] || [[ $arg == *wlength ]] )
+	then
+	    display_opt=$(echo $arg| cut -d'=' -f 2)
+	else
+	    echo "Error: invalid second argument."
+	    echo "Choose one from the list: --opt={alpha, num, freq, hist, wlength"
+	    exit
+	fi
+    fi
 fi
 
 
-#====================================
+#======================================
 # statistics on letter occurrences
-#====================================
+#======================================
 
-echo "Display the number of occurrences of each letter"
+case $display_opt in
+    "alpha")
+        echo "Display the number of occurrences of each letter"
 
-# count the number of occurrences of each letter in the file (how many words contain that letter)
-# - for		  => each letter (upper mode) in the alphabet (sequence A..Z)
-# - grep	  => search for the given letter
-# - wc -l	  => count the number of matching lines
-# - printf "%7d"  => print the given numerical variable right aligned (width of 7)
+	# count the number of occurrences of each letter in the file (how many words contain that letter)
+	# - for		  => each letter (upper mode) in the alphabet (sequence A..Z)
+	# - grep	  => search for the given letter
+	# - wc -l	  => count the number of matching lines
+	# - printf "%7d"  => print the given numerical variable right aligned (width of 7)
 
-for letter in {A..Z}
-do
-    letter_count=$(grep $letter $dico_file | wc -l)
-    printf " $letter - %7d\n" $letter_count 
-done
+	for letter in {A..Z}
+	do
+	    letter_count=$(grep $letter $dico_file | wc -l)
+	    printf " $letter - %7d\n" $letter_count 
+	done
+
+	;;
+    "num")
+	;;
+    "freq")
+        ;;
+    "hist")
+    	;;
+    "wlength")
+    	;;
+    *)
+        echo "Error: unknown display option".
+	echo "Choose one from the list: --opt={alpha, num, freq, hist, wlength"
+	exit
+	;;
+esac 
 
 echo ""
 
